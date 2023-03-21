0f365bfa1b3e720354124f569253bcb304341e31
Ioannis-Panagiotis Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2023-03-21 23:58:27 +0200

    Another Test

--- src/gr/aueb/cf/ch2/MathApp.java
+++ src/gr/aueb/cf/ch2/MathApp.java
@@ -10,6 +10,7 @@ public class MathApp {
 
         //The Math class
         //This is a test
+        //This is another test:)
         System.out.println("The absolute value of -5 is: " + Math.abs(-5));
         System.out.println("The minimum value between 1 and 10 is: " + Math.min(1,10));
         System.out.println("The maximum value between 1 and 10 is: " + Math.max(1,10));
